b9f46077381353787be9a22c77e88991e4f60563
manudous <manudous@gmail.com>
2023-08-02 12:13:13 +0200

    add react and vite

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <>
+      <h1>Bootcamps JS - React JavaScript SandBox</h1>
+    </>
+  );
+};
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import { App } from "./app";
+
+import "./style.css";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(<App />);
